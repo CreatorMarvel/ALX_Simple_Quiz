1c23460d2f96979d36223bb7d63e3d3d0c8283b8
Lawrence Mile <Laurancemile@gmail.com>
2024-09-30 11:29:21 +0200

    Add new files

--- quiz.js
+++ quiz.js
@@ -1,16 +1,21 @@
-const btnEl = document.querySelector("button");
-const feedbackEl = document.querySelector(".feedback");
-const form = document.querySelector("form");
-const correcttAnswer = 4;
+const btnEl = document.getElementById("submit-answer");
+const feedbackEl = document.getElementById("feedback");
 
-const checkAnswer = (answer) => {
-	correcttAnswer === answer
+const checkAnswer = () => {
+	const correctAnswer = 4;
+	const radios = document.getElementsByName("quiz");
+	let userAnswer;
+
+	for (const radio of radios) {
+		if (radio.checked) {
+			userAnswer = radio.value;
+			break;
+		}
+	}
+
+	correctAnswer == userAnswer
 		? (feedbackEl.textContent = "Correct! Well done.")
 		: (feedbackEl.textContent = "That's incorrect. Try again!");
 };
 
-btnEl.addEventListener("click", (e) => {
-	const userAnswer = new FormData(form).get("answer");
-	checkAnswer(Number(userAnswer));
-	e.preventDefault();
-});
+btnEl.addEventListener("click", checkAnswer);
