1ec5c39fc6d310035318939d0e62dc7ad43e9091
Lawrence Mile <Laurancemile@gmail.com>
2024-09-30 10:41:19 +0200

    Add correctAnswer

--- quiz.js
+++ quiz.js
@@ -1,9 +1,9 @@
 const btnEl = document.querySelector("button");
 const feedbackEl = document.querySelector(".feedback");
 const form = document.querySelector("form");
+const correcttAnswer = 4;
 
 const checkAnswer = (answer) => {
-	const correcttAnswer = 4;
 	correcttAnswer === answer
 		? (feedbackEl.textContent = "Correct! Well done.")
 		: (feedbackEl.textContent = "That's incorrect. Try again!");
